7c849b8361324b1a8460af2710a3b641195b9288
Ishwarya J S <juttuishwarya@gmail.com>
2026-02-17 21:34:18 +0530

    Update ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -6,9 +6,7 @@ total_cost = float(input("Enter the cost of your dream home: "))
 current_savings = 0.0      # Initial savings is zero
 ar = 0.04                   # Annual return on investment is 4% 
 months = 0
-
-# This is my comments
-#montly calculation
+#montly calculation (total)
 down_payment = total_cost * 0.25 # 25% down payment from total cost
 monthly_salary = annual_salary / 12
 monthly_deposit = monthly_salary * portion_saved  #portion of salary saved every month
@@ -17,5 +15,6 @@ while current_savings < down_payment:
     current_savings += current_savings * (ar / 12) #adding invest return for each month
     current_savings += monthly_deposit  #adding monthly salary savings of each month to total savings
     months += 1
+
 
 print(f"Number of months: {months}")
