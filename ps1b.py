6d7d662111f99b50da08914125fc867c8a20ad9d
Ishwarya J S <juttuishwarya@gmail.com>
2026-02-17 21:25:16 +0530

    Remove unnecessary line breaks in ps1b.py

--- ps1b.py
+++ ps1b.py
@@ -2,7 +2,7 @@
 annual_salary = float(input("Enter your starting annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
-semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: ")) # [cite: 38]
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
 
 portion_down_payment = 0.25
 current_savings = 0.0
