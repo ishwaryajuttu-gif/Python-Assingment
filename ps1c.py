1a4fcf569b6c00586d7882de83ef801beef58d8c
Ishwarya J S <juttuishwarya@gmail.com>
2026-02-17 21:23:58 +0530

    Refactor variables and improve code readability

--- ps1c.py
+++ ps1c.py
@@ -1,29 +1,31 @@
 starting_salary = float(input("Enter the starting salary: "))
-DOWN_PAYMENT = 1000000 * 0.25
-RAISE = 0.07
+down_payment = 1000000 * 0.25
+Raise = 0.07
 ROI = 0.04
-MONTHS = 36
+months = 36
 
-def check_savings(rate):
+def check_savings(rate):       #creating function to make a loop 
     savings = 0.0
     salary = starting_salary
-    for m in range(1, MONTHS + 1):
+    for m in range(1, months + 1):
         savings += savings * (ROI / 12) + (salary / 12) * (rate / 10000)
-        if m % 6 == 0: salary += salary * RAISE
+        if m % 6 == 0: salary += salary * Raise
     return savings
 
-low, high, steps = 0, 10000, 0
+low=0 
+high=10000
+steps = 0
 while low <= high:
     steps += 1
     mid = (low + high) // 2
     total_saved = check_savings(mid)
     
-    if abs(total_saved - DOWN_PAYMENT) <= 100: # Within $100 accuracy [cite: 56]
+    if abs(total_saved - down_payment) <= 100: 
         print(f"Best savings rate: {mid/10000:.4f}\nSteps in bisection search: {steps}")
         break
-    elif total_saved < DOWN_PAYMENT: low = mid + 1
+    elif total_saved < down_payment: low = mid + 1
     else: high = mid - 1
     
-    if low > 10000: # Impossible case [cite: 58]
+    if low > 10000: 
         print("It is not possible to pay the down payment in three years.")
         break
